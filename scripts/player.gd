f32f821152a738152b01b81df8542d7e67f88499
Jaron Li <2002jaron@gmail.com>
2024-02-20 20:23:02 -0800

    added sound

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,8 +6,11 @@ extends CharacterBody2D
 
 
 var canControl: bool = true
+var jumpSound
 
-
+func _ready():
+	jumpSound = $jump
+	
 func _physics_process(delta):
 	#check for death
 	if not canControl: 
@@ -20,6 +23,7 @@ func _physics_process(delta):
 	
 	if (Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("jump")) && is_on_floor():
 		velocity.y = -jpForce
+		jumpSound.play()
 	
 	if is_on_floor():
 		velocity.x = 100;
