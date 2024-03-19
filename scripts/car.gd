aa7f39540e2d8a721956d7707c82653def5fa4dc
Jaron Li <2002jaron@gmail.com>
2024-03-19 15:39:54 -0700

    added restart button to level 4

--- scripts/car.gd
+++ scripts/car.gd
@@ -7,3 +7,7 @@ func _process(delta):
 	# Move left
 	velocity.x = -100 # Adjust the speed as needed
 	move_and_collide(velocity * delta)
+
+func _on_attack_area_body_entered_car(body):
+	if body is Player:
+		body.handle_danger()
